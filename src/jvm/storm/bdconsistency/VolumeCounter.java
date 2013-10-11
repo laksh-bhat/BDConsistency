866efa8b62e4337ecab5b47284b228a6930faeb9
lbhat1 <laksh85@gmail.com>
2013-10-11 14:23:40 -0400

    aggregate volume for 1 day of trading

---
+++ src/jvm/storm/bdconsistency/VolumeCounter.java
@@ -0,0 +1,78 @@
+package bdconsistency;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.LocalDRPC;
+import backtype.storm.generated.StormTopology;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import bdconsistency.ask.AsksStateFactory;
+import bdconsistency.ask.AsksUpdater;
+import bdconsistency.bid.BidsStateFactory;
+import bdconsistency.bid.BidsUpdater;
+import bdconsistency.query.AsksBidsJoin;
+import bdconsistency.query.AxFinderFilter;
+import bdconsistency.query.BrokerEqualityQuery;
+import bdconsistency.query.PrinterBolt;
+import bdconsistency.trade.Trade;
+import storm.trident.Stream;
+import storm.trident.TridentState;
+import storm.trident.TridentTopology;
+import storm.trident.operation.BaseAggregator;
+import storm.trident.operation.CombinerAggregator;
+import storm.trident.operation.ReducerAggregator;
+import storm.trident.operation.TridentCollector;
+import storm.trident.spout.ITridentSpout;
+import storm.trident.spout.RichSpoutBatchExecutor;
+import storm.trident.testing.MemoryMapState;
+import storm.trident.tuple.TridentTuple;
+
+/**
+ * User: lbhat@damsl
+ * Date: 10/11/13
+ * Time: 1:47 PM
+ */
+public class VolumeCounter {
+    public static class VolumeAggregator implements ReducerAggregator<VolumeAggregator.CountState> {
+        @Override
+        public CountState init() {
+            return new CountState();
+        }
+
+        @Override
+        public CountState reduce(CountState state, TridentTuple tuple) {
+            Trade t = new Trade(tuple.getString(0).split("\\|"));
+            if (t.getOperation() == 1) state.count += t.getVolume();
+            else state.count -= t.getVolume();
+            return state;
+        }
+
+        public class CountState {
+            long count = 0;
+        }
+    }
+
+    public static StormTopology buildTopology(String fileName) {
+        TridentTopology topology = new TridentTopology();
+        final ITridentSpout asksSpout = new RichSpoutBatchExecutor(new FileStreamingSpout(fileName));
+
+        Stream asks = topology
+                .newStream("spout1", asksSpout)
+                .shuffle()
+                .parallelismHint(5)
+                .aggregate( new VolumeAggregator(), new Fields("volume"))
+                .each(new Fields("volume"), new PrinterBolt());
+        return topology.build();
+    }
+
+    public static void main(String[] args) {
+        Config conf = new Config();
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("VolumeCounterTopology", conf, buildTopology(args[0]));
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ignore) {}
+        cluster.killTopology("VolumeCounterTopology");
+        cluster.shutdown();
+    }
+}
