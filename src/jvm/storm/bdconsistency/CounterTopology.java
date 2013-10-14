e3c32b2fe8a7b01130fcb2c3212b265c870f77ba
lbhat1 <laksh85@gmail.com>
2013-10-14 18:04:26 -0400

    counter reducer serialization implementation -- fixed serialization issue

--- src/jvm/storm/bdconsistency/CounterTopology.java
+++ src/jvm/storm/bdconsistency/CounterTopology.java
@@ -1,7 +1,6 @@
 package bdconsistency;
 
 import backtype.storm.Config;
-import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.DRPCExecutionException;
@@ -10,10 +9,7 @@ import backtype.storm.generated.StormTopology;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.DRPCClient;
-import bdconsistency.query.AsksBidsJoin;
-import bdconsistency.query.BrokerEqualityQuery;
 import bdconsistency.query.PrinterBolt;
-import bdconsistency.trade.Trade;
 import com.google.common.collect.Lists;
 import org.apache.thrift7.TException;
 import storm.trident.Stream;
@@ -23,12 +19,10 @@ import storm.trident.operation.*;
 import storm.trident.spout.ITridentSpout;
 import storm.trident.spout.RichSpoutBatchExecutor;
 import storm.trident.state.BaseQueryFunction;
-import storm.trident.state.State;
 import storm.trident.state.StateUpdater;
 import storm.trident.testing.MemoryMapState;
 import storm.trident.tuple.TridentTuple;
 
-import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -40,10 +34,11 @@ import java.util.Map;
  */
 public class CounterTopology {
 
-    public static class CountReducer implements StateUpdater<CounterState> {
+    public static class CountUpdater implements StateUpdater<CounterState> {
         @Override
         public void updateState(CounterState state, List<TridentTuple> tuples, TridentCollector collector) {
-            state.increment();
+            for (TridentTuple t : tuples)
+                state.increment();
         }
         @Override
         public void prepare(Map conf, TridentOperationContext context) {}
@@ -86,7 +81,7 @@ public class CounterTopology {
                 .newStream("spout", asksSpout);
 
         TridentState state = counts.shuffle()
-                .partitionPersist(new CounterState.CounterStateFactory(), new CountReducer(), new Fields("count"))
+                .partitionPersist(new CounterState.CounterStateFactory(), new CountUpdater(), new Fields("count"))
                 .parallelismHint(8);
 
         topology
@@ -106,7 +101,7 @@ public class CounterTopology {
                         collector.emit(new Values(result));
                     }
                 }, new Fields("count"))
-                .parallelismHint(5)
+                .parallelismHint(8)
                 .shuffle()
                 .project(new Fields("count"));
 
@@ -115,23 +110,18 @@ public class CounterTopology {
 
     public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException, InterruptedException, TException, DRPCExecutionException {
         Config conf = new Config();
-        conf.registerSerialization(MemoryMapState.class);
-        try {
-            conf.registerSerialization(Class.forName("storm.trident.testing.MemoryMapState.MemoryMapStateBacking"));
-        } catch (ClassNotFoundException ignore) {}
-
         conf.setFallBackOnJavaSerialization(true);
         conf.setNumWorkers(20);
         conf.put(Config.DRPC_SERVERS, Lists.newArrayList("damsel", "qp4", "qp5", "qp6"));
-        conf.setMaxSpoutPending(20);
+        conf.setMaxSpoutPending(2);
         conf.put(Config.STORM_CLUSTER_MODE, "distributed");
         StormSubmitter.submitTopology("CounterTopology", conf, buildTopology(args[0]));
         // wait for 2 minutes
-        Thread.sleep(60000);
+
 
         DRPCClient client = new DRPCClient("localhost", 3772);
         for (int i = 0; i < 20; i++) {
-            Thread.sleep(2000);
+            Thread.sleep(60000);
             System.out.println("Result for AXF query is -> " + client.execute("Counter", "find-count"));
         }
         client.close();
