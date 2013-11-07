cdbb7205315b319c81d9b4a3e87e7b0f0ba06169
lbhat1 <laksh85@gmail.com>
2013-11-07 14:41:27 -0500

    Tpch Query Implementation - change drpc server list

--- src/jvm/storm/bdconsistency/utils/PropertiesReader.java
+++ src/jvm/storm/bdconsistency/utils/PropertiesReader.java
@@ -21,7 +21,7 @@ public class PropertiesReader {
         conf.setMaxSpoutPending(1);
         conf.put("topology.spout.max.batch.size", 50);
         conf.put("topology.trident.batch.emit.interval.millis", 500 );
-        //conf.put(Config.DRPC_SERVERS, Lists.newArrayList("damsel", "qp4", "qp5", "qp6"));
+        conf.put(Config.DRPC_SERVERS, Lists.newArrayList("qp-hd1", "qp-hd9", "qp-hd3", "qp-hd4"));
         conf.put(Config.STORM_CLUSTER_MODE, "distributed");
         return conf;
     }
