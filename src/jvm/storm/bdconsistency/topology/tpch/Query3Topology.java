857cc3606f30457f6f02ac9a2c61399d507b077a
lbhat1 <laksh85@gmail.com>
2013-11-06 19:47:20 -0500

    Tpch Query Implementation - first draft - storm doesn't like input fields definition for aggregate(). remove it

--- src/jvm/storm/bdconsistency/topology/tpch/Query3Topology.java
+++ src/jvm/storm/bdconsistency/topology/tpch/Query3Topology.java
@@ -66,8 +66,8 @@ public class Query3Topology {
                 .stateQuery(tpchState, new TpchQuery.Query3(), new Fields("orderkey", "orderdate", "shippriority", "extendedprice", "discount"))
                 .parallelismHint(8)
                 .groupBy(new Fields("orderkey", "orderdate", "shippriority"))
-                .aggregate(new Fields("orderkey", "orderdate", "shippriority", "extendedprice", "discount")
-                        , new TpchQuery.Query3Aggregator()
+                .aggregate(/*new Fields("orderkey", "orderdate", "shippriority", "extendedprice", "discount")*/
+                        new TpchQuery.Query3Aggregator()
                         , new Fields("orderkey", "orderdate", "shippriority", "query3"))
         ;
 
