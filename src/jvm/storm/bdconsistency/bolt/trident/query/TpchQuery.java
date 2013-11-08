caf773eabc31d3a409f77f9bd40e90337f4fe39a
lbhat1 <laksh85@gmail.com>
2013-11-08 00:08:06 -0500

    modify query params

--- src/jvm/storm/bdconsistency/bolt/trident/query/TpchQuery.java
+++ src/jvm/storm/bdconsistency/bolt/trident/query/TpchQuery.java
@@ -89,7 +89,7 @@ public class TpchQuery {
             ITpchTable lineItem = state.getTable("lineitem");
 
             if (orders != null && customer != null && lineItem != null) {
-                filterCustomers(customer);
+                //filterCustomers(customer);
                 filterOrders(orders);
                 filterLineItems(lineItem);
                 computeIntermediateJoinResults(results, orders, customer, lineItem);
