fd4e46ea855fcd731e3961d4be7937d84a914e29
lbhat1 <laksh85@gmail.com>
2013-11-18 17:29:45 -0500

    More Query3 configuration changes - reduce batch size to 300

--- src/jvm/storm/bdconsistency/utils/PropertiesReader.java
+++ src/jvm/storm/bdconsistency/utils/PropertiesReader.java
@@ -19,7 +19,7 @@ public class PropertiesReader {
         conf.setNumAckers(8);
         conf.setNumWorkers(8);
         conf.setMaxSpoutPending(16);
-        conf.put("topology.spout.max.batch.size", 500);
+        conf.put("topology.spout.max.batch.size", 300);
         conf.put("topology.trident.batch.emit.interval.millis", 500 );
         conf.put(Config.DRPC_SERVERS, Lists.newArrayList("qp-hd1", "qp-hd9", "qp-hd3", "qp-hd4"));
         conf.put(Config.STORM_CLUSTER_MODE, "distributed");
